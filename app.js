1a50ccd7f91c9a62e7a98d3f72f9fb3428f081b2
Sean Connor <connorseanp@gmail.com>
2016-03-06 14:54:15 -0800

    server listen

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req,res) {
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT);
 console.log("Server Started.");
 
 
